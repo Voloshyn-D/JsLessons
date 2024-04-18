8769aa7f4e3fbffbff958868384a0e3807dad592
Voloshyn Denys <volosindenis8@gmail.com>
2024-04-18 14:34:39 +0300

    modified the file js.js

--- Lessons/js.js
+++ Lessons/js.js
@@ -5,4 +5,18 @@ let randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
 
 //alert("Сумістнісь імен " + firstName + " + " + secondName + " = " + randomNumber + "%");
-alert(`Сумістнісь імен ${firstName} + ${secondName} = ${randomNumber}%`);
+//alert(`Сумістнісь імен ${firstName} + ${secondName} = ${randomNumber}%`);
+
+if (randomNumber < 33) {
+    alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
+${randomNumber}% спробуйте пошукати інше ім'я`)
+}
+else if (randomNumber >= 33 && randomNumber < 66) {
+    alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
+${randomNumber}% сумістність хороша але може бути краще`)
+}
+else if (randomNumber > 66) {
+    alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
+${randomNumber}% Ідеальна сумістність`)
+}
+else { alert("Error") }
