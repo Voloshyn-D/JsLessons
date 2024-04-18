3ea882293d8afe1131bb9c8ba55bbe54c749b9ca
Voloshyn Denys <volosindenis8@gmail.com>
2024-04-18 15:57:39 +0300

    upgrade file js.js

--- Lessons/js.js
+++ Lessons/js.js
@@ -2,21 +2,24 @@ const firstName = prompt("Напишіть перше ім'я");
 const secondName = prompt("Напишіть друге ім'я");
 
 let randomNumber = Math.floor(Math.random() * 100);
-console.log(randomNumber);
+//console.log(randomNumber);
 
 //alert("Сумістнісь імен " + firstName + " + " + secondName + " = " + randomNumber + "%");
 //alert(`Сумістнісь імен ${firstName} + ${secondName} = ${randomNumber}%`);
 
-if (randomNumber < 33) {
+if (randomNumber < 33 && firstName != null && secondName != null 
+    && firstName !="" && secondName !="") {
     alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
 ${randomNumber}% спробуйте пошукати інше ім'я`)
 }
-else if (randomNumber >= 33 && randomNumber < 66) {
+else if (randomNumber >= 33 && randomNumber < 66 && firstName != null 
+    && secondName != null && firstName !="" && secondName !="") {
     alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
 ${randomNumber}% сумістність хороша але може бути краще`)
 }
-else if (randomNumber > 66) {
+else if (randomNumber > 66 && firstName != null && secondName != null 
+    && firstName !="" && secondName !="") {
     alert(`Результат сумістності імен ${firstName} і ${secondName} дорівнює
 ${randomNumber}% Ідеальна сумістність`)
 }
-else { alert("Error") }
+else { alert("Error Спробуйте ввести імена знову") }
