7a12a956d236b6ff5dbd03d4d8547f20ea2c074b
Voloshyn Denys <volosindenis8@gmail.com>
2024-04-23 15:47:14 +0300

    add jsArray.js file

---
+++ Lessons/Block2/jsArray.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*const userObject = {
+    firstName: "John",
+    age: 30,
+}
+console.log(userObject)*/
+
+const userArr = ["John", 30, 20, "UA"]
+
+
+/*userArr.push("mail")               // методы масива
+userArr[0] = "Anna"
+userArr.unshift(10)
+userArr.pop()
+userArr.shift()
+userArr.splice(1, 2, 25, "feamal")
+console.log(userArr.includes(30))*/
+console.log(userArr.indexOf(30))
+
+console.log(userArr)
+
