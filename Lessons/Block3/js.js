aaa4d77a32bc1c6137d1ef4005eb60e0b847ea6c
Voloshyn Denys <volosindenis8@gmail.com>
2024-04-25 18:58:20 +0300

    change two files index.html js.js

---
+++ Lessons/Block3/js.js
@@ -0,0 +1,11 @@
+'use strict'
+/*
+let header = document.querySelector(".header")
+let navList = document.querySelector(".nav__list")
+
+console.log(header.firstChild);
+console.log(navList.childNodes);
+*/
+let navItems = document.querySelectorAll("li")
+
+console.log(navItems);
