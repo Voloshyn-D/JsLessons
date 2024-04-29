e38fbd065a23542f496e597f7a7edfb957f8842d
Voloshyn Denys <volosindenis8@gmail.com>
2024-04-29 18:24:57 +0300

    change index.html and js.js files

--- Lessons/Block3/js.js
+++ Lessons/Block3/js.js
@@ -6,6 +6,14 @@ let navList = document.querySelector(".nav__list")
 console.log(header.firstChild);
 console.log(navList.childNodes);
 */
-let navItems = document.querySelectorAll("li")
+let elem = document.querySelector("h1")
+let oldDate = elem.innerHTML;
 
-console.log(navItems);
+//console.log(elem.innerHTML);
+
+//elem.innerHTML = `<p>${oldDate}</p> <p>Hello! i am a new text</p>`
+elem.outerHTML = `<p>Hello! I am a new text</p>`
+
+console.log(elem.firstChild);
+
+elem.firstChild.data = "I am a new comment"
